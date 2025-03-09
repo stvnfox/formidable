689c52d237a57a62de2016e8d47d8ab4392a7aea
steven.vos <steven.vos@iodigital.com>
2025-03-09 15:46:38 +0100

    refactor(account-menu): adjusted account menu styling

--- components/layout/account-menu.vue
+++ components/layout/account-menu.vue
@@ -19,12 +19,20 @@ const items = ref([
 
 <template>
   <UDropdownMenu
+    size="sm"
     :items="items"
+    :content="{
+      align: 'end',
+    }"
     :ui="{
-      content: 'w-48',
+      content: 'w-32 font-bold',
     }"
   >
-    <UButton color="neutral" class="gap-2" variant="ghost">
+    <UButton
+      color="neutral"
+      class="group gap-3 hover:bg-transparent cursor-pointer hover:text-neutral-300"
+      variant="ghost"
+    >
       <span class="font-bold">{{ $auth.user?.given_name }}</span>
       <UAvatar :src="$auth.user?.picture ?? undefined" />
     </UButton>
