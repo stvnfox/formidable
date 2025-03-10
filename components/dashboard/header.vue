d8e4623a813e242b54bc4fcc4cdc00ab134ed360
steven.vos <steven.vos@iodigital.com>
2025-03-10 20:32:49 +0100

    style(header): use slots for description and title for styling purposes

--- components/dashboard/header.vue
+++ components/dashboard/header.vue
@@ -2,11 +2,16 @@
   <UAlert
     color="neutral"
     variant="subtle"
-    :title="`hi, ${$auth.user?.given_name}!`"
-    description="it's a great day to build some formidable forms - see your forms below or create a new one"
     icon="i-lucide-handshake"
     :ui="{
         icon: 'text-neutral-500',
     }"
-  />
+  >
+    <template #title>
+      Hi, <span class="capitalize">{{ $auth.user?.given_name }}</span>
+    </template>
+    <template #description>
+      It's a great day to build some formidable forms - see your forms below or create a new one
+    </template>
+  </UAlert>
 </template>
