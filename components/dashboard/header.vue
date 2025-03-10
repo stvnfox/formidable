02f4e7a34d85b64e3a9c4624cd7eab1f3221235c
steven.vos <steven.vos@iodigital.com>
2025-03-10 20:27:24 +0100

    feat(header): change header

--- components/dashboard/header.vue
+++ components/dashboard/header.vue
@@ -1,13 +1,12 @@
 <template>
-  <div class="px-6 py-5 border border-neutral-700 rounded-lg bg-neutral-900">
-    <h1 class="text-xl">
-      Hi,
-      <span class="capitalize">{{ $auth.user?.given_name }}!</span>
-    </h1>
-    <p class="text-sm max-w-md text-pretty">
-      Welcome back! It's a great day to build some
-      <span class="font-heading text-xl">formidable</span> forms. See your forms
-      below or create a new one!
-    </p>
-  </div>
+  <UAlert
+    color="neutral"
+    variant="subtle"
+    :title="`hi, ${$auth.user?.given_name}!`"
+    description="it's a great day to build some formidable forms - see your forms below or create a new one"
+    icon="i-lucide-handshake"
+    :ui="{
+        icon: 'text-neutral-500',
+    }"
+  />
 </template>
