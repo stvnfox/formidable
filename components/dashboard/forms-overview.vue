b561cb4e740f3035c1be0159d89d6b7b30f6847b
steven.vos <steven.vos@iodigital.com>
2025-03-11 10:32:19 +0100

    refactor(forms overview): changed forms-overview styling etc

--- components/dashboard/forms-overview.vue
+++ components/dashboard/forms-overview.vue
@@ -1,4 +1,6 @@
 <script setup lang="ts">
+const { data, status } = await useGetForms();
+
 const items = [
   {
     label: "Icons",
@@ -21,12 +23,21 @@ const items = [
       <h2 class="text-3xl font-heading">Your forms</h2>
       <UButton>Create new form</UButton>
     </div>
-    <div class="mt-4 border border-neutral-700 rounded-lg bg-neutral-900 px-6 py-2">
+    <div
+      v-if="data.length > 0"
+      class="mt-4 border border-neutral-700 rounded-lg bg-neutral-900 px-6 py-2"
+    >
       <UAccordion :items="items">
         <template #body="{ item }">
           This is the {{ item.label }} panel.
         </template>
       </UAccordion>
+    </div>
+    <div v-else class="text-sm py-8">
+      <h3 class="text-sm font-bold text-neutral-300">No forms found</h3>
+      <p class="text-neutral-400">
+        Create a new form to get started by clicking the button above
+      </p>
     </div>
   </div>
 </template>
