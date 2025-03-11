e1425b367c562a2e291ff6382d20b95fe8e96178
steven.vos <steven.vos@iodigital.com>
2025-03-11 19:00:31 +0100

    chore(form detail page): init form detail page w/ dummy data

---
+++ pages/form/[id].vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+const { id } = useRoute().params;
+
+console.log(id);
+
+// const { data } = await useGetForm(id);
+</script>
+
+<template>
+  <div>
+    <h1>Form</h1>
+  </div>
+</template>
